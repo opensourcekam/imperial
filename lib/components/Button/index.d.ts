7ad38036c804aaa22c2c0d4bc85bf44f631dead5
Kameron Robinson <kamdroidR@me.com>
2019-01-27 22:51:31 -0500

    add textarea

--- lib/components/Button/index.d.ts
+++ lib/components/Button/index.d.ts
@@ -1,2 +1,2 @@
-declare const Button: import("styled-components").StyledComponent<"button", any, {}, never>;
+declare const Button: import("styled-components").StyledComponent<"button", import("../../theme").default, {}, never>;
 export default Button;
